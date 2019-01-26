1b77c8850bd3321b8ca220d4cc299f775a95f13d
Alef Carlos <alef.carlos@gmail.com>
2019-01-26 19:27:04 -0200

    feat: adicionado dismiss widgter na lista de coleções

--- lib/pages/collections/collections.dart
+++ lib/pages/collections/collections.dart
@@ -43,29 +43,63 @@ class CollectionsPage extends StatelessWidget {
     );
   }
 
+  Widget _buildListTileDismissBackground() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10.0),
+      color: Colors.red,
+      alignment: Alignment.centerRight,
+      child: Text(
+        'Excluir coleção',
+        textAlign: TextAlign.right,
+        style: TextStyle(
+            color: Colors.white, fontWeight: FontWeight.bold, fontSize: 26.0),
+      ),
+    );
+  }
+
+  Widget _buildButtonBar(BuildContext context, int index) {
+    return ButtonBar(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.delete_forever,
+            color: Colors.red,
+          ),
+          tooltip: 'Excluir coleção',
+          onPressed: () => _showConfirmDeletion(context, index),
+        )
+      ],
+    );
+  }
+
   Widget _buildListTile(BuildContext context, int index) {
     var item = collections[index];
 
-    return ListTile(
-      onTap: () => Application.router.navigateTo(context, '/collection/$index',
-          transition: TransitionType.inFromRight),
-      title: Text(item.name),
-      subtitle: Text('10 de ${item.itemCount}'),
-      leading: Icon(
-        item.isFav ? Icons.favorite : Icons.favorite_border,
-        color: item.isFav ? Colors.red : null,
-      ),
-      trailing: ButtonBar(
-        mainAxisSize: MainAxisSize.min,
+    return Dismissible(
+      key: Key(item.name),
+      direction: DismissDirection.endToStart,
+      background: _buildListTileDismissBackground(),
+      onDismissed: (DismissDirection direction) {
+        if (direction == DismissDirection.endToStart) {
+          deleteCollection(index);
+        }
+      },
+      child: Column(
         children: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.delete_forever,
-              color: Colors.red,
+          ListTile(
+            onTap: () => Application.router.navigateTo(
+                context, '/collection/$index',
+                transition: TransitionType.inFromRight),
+            title: Text(item.name),
+            subtitle: Text('10 de ${item.itemCount}'),
+            leading: Icon(
+              item.isFav ? Icons.favorite : Icons.favorite_border,
+              color: item.isFav ? Colors.red : null,
             ),
-            tooltip: 'Excluir coleção',
-            onPressed: () => _showConfirmDeletion(context, index),
-          )
+            trailing: _buildButtonBar(context, index),
+          ),
+          Divider()
         ],
       ),
     );
