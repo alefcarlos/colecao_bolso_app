081e67b952e23ebf8ef6d77be973ab3961a1d5b2
Alef Carlos <alef.carlos@gmail.com>
2019-03-02 23:08:33 -0300

    feat: cached_network_image package.

--- lib/collection/list/collection_item_grid_item.dart
+++ lib/collection/list/collection_item_grid_item.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../collection_item_model.dart';
 import '../../common/common.dart';
 import '../bloc/list/exporter.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class CollectionItemGridItem extends StatelessWidget {
   final CollectionItem _item;
@@ -14,8 +15,10 @@ class CollectionItemGridItem extends StatelessWidget {
     return Card(
       child: Column(
         children: [
-          Image.asset(
-            'assets/food.jpg',
+          new CachedNetworkImage(
+            imageUrl: "http://via.placeholder.com/350x150",
+            placeholder: (context, url) => new CircularProgressIndicator(),
+            errorWidget: (context, url, error) => new Icon(Icons.error),
           ),
           ListTile(
             title: Text('#${_item.number}'),
